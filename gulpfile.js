68aad39224108f222891999464dd1a72e0702140
OmarHegazi94 <edgeofdarkness_x1@hotmail.com>
2020-01-26 14:20:21 +0200

    fixed node issue + don autoprfix

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 const gulp = require('gulp');
 const gulpLess = require('gulp-less');
+const autoprefixer = require('gulp-autoprefixer');
 
     gulp.task('compile-less', () => {
         gulp.src('./assets/style/css/*.less')
+        // compile less => css
         .pipe(gulpLess())
+        // add autoprefixer
+        .pipe(autoprefixer())
+        // minify css 
+
+        // bundle all css
+        
         .pipe(gulp.dest('./dist/style/css'))
     });
 
-    
